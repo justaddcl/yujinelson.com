6516921e74edb5bdb2853336daaf923d11fbca17
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-19 15:48:34 -0500

    created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <span>©2020 Yuji Nelson. Made with ❤️ in Austin, TX. Soli deo gloria</span>
+  </footer>
+);
+
+export default Footer;
