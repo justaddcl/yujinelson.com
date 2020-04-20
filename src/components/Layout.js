a5737427a3bd0d844ee45335c37cbfc0e347f6ea
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-19 23:35:07 -0500

    layout - added global styles, theme, container

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Header from './Header';
 import Footer from './Footer';
+import GlobalStyles from './styles/GlobalStyles';
 import 'normalize.css';
+import ContentContainer from './styles/ContentContainer';
+import theme from './styles/theme';
+
+const LayoutContainer = styled(ContentContainer)`
+  display: grid;
+  grid-template-rows: 48px 1fr 32px;
+
+  @media screen and (min-width: ${theme.mq.desktop.small}) {
+    grid-template-rows: 80px 1fr 64px;
+  }
+`;
 
 const Layout = ({ children }) => (
   <div>
