ec955e90f22a53079c17b2cb683f2a4a4b1c9ece
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-19 23:36:19 -0500

    footer - added basic styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import theme from './styles/theme';
+
+const StyledFooter = styled.footer`
+  border-top: solid 1px ${theme.colors.purple500};
+  display: flex;
+  flex-direction: row;
+  height: 32px;
+`;
 
 const Footer = () => (
-  <footer>
-    <span>©2020 Yuji Nelson. Made with ❤️ in Austin, TX. Soli deo gloria</span>
-  </footer>
+  <StyledFooter>
+    <span>©2020 Yuji Nelson. Made with ❤️ in Austin, TX.</span>
+    <span>Soli deo gloria</span>
+  </StyledFooter>
 );
 
 export default Footer;
