0c99d06e09c39419658950ee507d01360c62729f
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-19 23:22:47 -0500

    created theme variables

---
+++ src/components/styles/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  colors: {
+    background: '#3E3C53',
+    font: '#D8DEE9',
+    blue900: '#3B4252',
+    purple500: '#B48EAD',
+    purple900: '#3E3C53',
+  },
+  font: {
+    family: 'source-code-pro, monospace',
+  },
+  mq: {
+    desktop: {
+      small: '1024px',
+    },
+  },
+  padding: {
+    mobile: {
+      medium: '24px',
+    },
+  },
+};
+
+export default theme;
