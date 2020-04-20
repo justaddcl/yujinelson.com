cfb144ec9e7f19aecf506216bd2a8e3310c80266
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-19 23:36:06 -0500

    header - added basic styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+import theme from './styles/theme';
+
+const StyledHeader = styled.header`
+  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 48px;
+
+  @media screen and (min-width: ${theme.mq.desktop.small}) {
+    height: 80px;
+  }
+`;
+
+const StyledNav = styled.nav`
+  justify-self: end;
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+
+    li {
+      margin-left: 16px;
+    }
+  }
+`;
 
 const Header = () => (
-  <header>
-    <span>
+  <StyledHeader>
+    <div>
       <Link to="/">Yuji Nelson</Link>
-    </span>
-    <ul>
-      <li>
-        <Link to="/about">Aboot Me</Link>
-      </li>
-    </ul>
-  </header>
+    </div>
+    <StyledNav>
+      <ul>
+        <li>
+          <Link to="/about">/about</Link>
+        </li>
+        <li>
+          <Link to="/uses">/uses</Link>
+        </li>
+      </ul>
+    </StyledNav>
+  </StyledHeader>
 );
 
 export default Header;
