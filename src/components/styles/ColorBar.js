2d8eb1e8d451a2c11bc3efaf817191fc4ede8411
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-19 23:30:40 -0500

    created ColorBar component

---
+++ src/components/styles/ColorBar.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import theme from './theme';
+
+const ColorBar = styled.div`
+  background-color: ${theme.colors.purple500};
+  height: 4px;
+  width: 100%;
+`;
+
+export default ColorBar;
