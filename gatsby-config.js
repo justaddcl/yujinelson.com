689a85441039f3ebf33af4eceaa88c023c76f62b
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-20 23:33:54 -0500

    gatsby-config - added gatsby-plugin-react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-layout`,
