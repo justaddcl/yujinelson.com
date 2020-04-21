9f5207bbe63b45496dabea61b40b79575a48d3f2
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-20 23:35:47 -0500

    index page - added Head component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Head from '../components/Head';
 
-const Home = () => <h1>It's a page</h1>;
+const Home = () => {
+  return (
+    <>
+      <Head />
+      <h1>It's a page</h1>
+    </>
+  );
+};
 
 export default Home;
