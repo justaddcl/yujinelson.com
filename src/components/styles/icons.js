bf00d91d547943fc44175c1cc9cee96addc4bc9a
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-27 00:46:10 -0500

    created theme / icons for fa icons used

---
+++ src/components/styles/icons.js
@@ -0,0 +1,22 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faHeart, faTimes } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faDribbble,
+  faCodepen,
+  faLastfm,
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  faBars,
+  faHeart,
+  faTimes,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faDribbble,
+  faCodepen,
+  faLastfm
+);
