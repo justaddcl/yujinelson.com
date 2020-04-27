945cf822c0dcb2679ded74bf708529117de96ce5
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-27 00:46:24 -0500

    theme / padding - separated out values into separate file

---
+++ src/components/styles/padding.js
@@ -0,0 +1,13 @@
+const padding = {
+  mobile: {
+    small: '24px',
+    medium: '48px',
+    large: '64px',
+  },
+  desktop: {
+    medium: '96px',
+    large: '128px',
+  },
+};
+
+export default padding;
