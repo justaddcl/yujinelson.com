4904969c1de6babbfe4e69b9fdcf53e9db9f7c2f
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-27 00:51:43 -0500

    GlobalStyles - added css reset, updated references to theme values

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -1,15 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
-import theme from './theme';
+import 'reset-css';
 import 'normalize.css';
+import theme, { colors, fonts } from './theme';
 
 const GlobalStyles = createGlobalStyle`
   @import url("https://use.typekit.net/xoo3bkz.css");
   @import url("https://use.typekit.net/kum8wap.css");
 
   * {
-    background-color: ${theme.colors.background};
-    color: ${theme.colors.font};
-    font-family: ${theme.font.family};
+    background-color: ${colors.purple[800]};
+    color: ${colors.grey[300]};
+    font-family: ${fonts.family.sourceCodePro};
   }
 `;
 
