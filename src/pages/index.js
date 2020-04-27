148967190af5b64f6f5bd47faf1a5256d4ad1c1d
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-27 00:58:23 -0500

    index - using ContentContainer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Head from '../components/Head';
+import ContentContainer from '../components/styles/ContentContainer';
 
 const Home = () => {
   return (
-    <>
+    <ContentContainer>
       <Head />
       <h1>It's a page</h1>
-    </>
+    </ContentContainer>
   );
 };
 
