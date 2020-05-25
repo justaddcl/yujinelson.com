fa867e3f51d54812ea32ed4b7b7ef6c3549896f8
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-05-25 18:42:28 -0500

    ContentContainer - updated reference to theme.mq

--- src/components/styles/ContentContainer.js
+++ src/components/styles/ContentContainer.js
@@ -5,7 +5,7 @@ const ContentContainer = styled.div`
   padding-left: ${spacing.s}rem;
   padding-right: ${spacing.s}rem;
 
-  @media screen and (min-width: ${theme.mq.desktop.small}) {
+  @media screen and (min-width: ${mq.desktop.small}) {
     max-width: ${theme.sizes.desktop.maxWidth};
     margin: 0 auto;
   }
