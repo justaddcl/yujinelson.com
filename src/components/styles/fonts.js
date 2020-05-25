1e7051536170ad3204a03e1c0c2d91daada92191
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-05-25 16:53:48 -0500

    theme / fonts - added responsive font size

--- src/components/styles/fonts.js
+++ src/components/styles/fonts.js
@@ -1,8 +1,15 @@
+const BASE = 16;
+
 const fonts = {
   family: {
     proximaNova: 'proxima-nova, sans-serif',
     sourceCodePro: 'source-code-pro, monospace',
   },
+  size: {
+    mobile: BASE,
+    desktop: BASE * 1.125,
+  },
 };
 
 export default fonts;
+export { BASE };
