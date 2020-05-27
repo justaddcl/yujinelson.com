915e7340899e5c46e9eee098a9419175180e9e88
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-05-26 22:20:38 -0500

    MobileMenu - updated styles to match design

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
-import { colors, mq } from './styles/theme';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { colors, fonts, mq, spacing } from './styles/theme';
 import ContentContainer from './styles/ContentContainer';
-import SocialMediaIcons from './SocialMediaIcons';
 
 const StyledMobileMenu = styled.nav`
-  background-color: ${colors.purple[900]};
+  background-color: ${colors.purple[800]};
   bottom: 0;
   height: calc(100vh - 76px);
-  padding: 48px 0;
+  opacity: 0;
   position: fixed;
   transform: translateX(100%);
   width: 100vw;
   z-index: 1;
-  transition: 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+  transition: opacity 0.15s cubic-bezier(0.86, 0, 0.07, 1),
+    transform 0.2s cubic-bezier(0.4, 0, 0.2, 1);
 
   ul {
     display: flex;
@@ -22,37 +23,83 @@ const StyledMobileMenu = styled.nav`
     list-style: none;
 
     li {
-      margin-bottom: 48px;
+      &.nav-item {
+        margin-bottom: ${spacing.l}rem;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+
+      &.mb-xl {
+        margin-bottom: ${spacing.xl}rem;
+      }
 
       a {
-        color: ${colors.grey[300]};
+        color: ${colors.grey[100]};
+        font-family: ${fonts.family.sourceCodePro};
+        font-size: 1.5rem;
         text-decoration: none;
+
+        &.button--primary {
+          background-color: ${colors.purple[500]};
+          color: ${colors.grey[100]};
+          display: grid;
+          column-gap: ${spacing.base}rem;
+          grid-template-columns: repeat(2, auto);
+          font-family: ${fonts.family.proximaNova};
+          font-size: 20px;
+          padding: ${spacing.base}rem ${spacing.s}rem;
+
+          @media screen and (min-width: 375px) {
+            padding: ${spacing.base}rem ${spacing.m}rem;
+          }
+
+          & .icon {
+            font-size: 0.875rem;
+          }
+        }
       }
     }
   }
 
-  &.open {
+  &.is-open {
+    opacity: 1;
     transform: translateX(0);
-    transition: transform 0.25s cubic-bezier(0, 0, 0.2, 1);
+    transition: opacity 0.15s cubic-bezier(0.86, 0, 0.07, 1),
+      transform 0.25s cubic-bezier(0, 0, 0.2, 1);
+
+    @media screen and (min-width: ${mq.desktop.small}) {
+      display: none;
+    }
   }
+`;
+
+const MobileMenuContainer = styled(ContentContainer)`
+  padding: ${spacing.xl}rem ${spacing.m}rem;
 `;
 
 const MobileMenu = () => (
   <StyledMobileMenu>
-    <ContentContainer>
+    <MobileMenuContainer>
       <ul>
-        <li>
+        <li className="nav-item">
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/about">/about</Link>
+        <li className="nav-item">
+          <Link to="/about">About</Link>
         </li>
-        <li>
-          <Link to="/uses">/uses</Link>
+        <li className="nav-item mb-xl">
+          <Link to="/">Contact</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="button--primary">
+            <span>Download resume</span>
+            <FontAwesomeIcon icon="download" className="icon" />
+          </Link>
         </li>
       </ul>
-      <SocialMediaIcons />
-    </ContentContainer>
+    </MobileMenuContainer>
   </StyledMobileMenu>
 );
 
