19f9b06a61c9fb6f3eef740629e521306ce8413e
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-05-27 00:28:24 -0500

    Footer / desktop - updated markup, styles to match design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,15 @@ const StyledFooter = styled.footer`
   }
 
   .nav {
-    display: flex;
-    flex-direction: column;
+    display: grid;
+    grid-auto-flow: row;
     gap: ${spacing.base}rem;
+
+    @media screen and (min-width: ${mq.desktop.small}) {
+      justify-self: center;
+      grid-auto-flow: column;
+      gap: ${spacing.xl}rem;
+    }
 
     & a {
       color: ${colors.grey[200]};
@@ -32,8 +38,28 @@ const StyledFooter = styled.footer`
     }
   }
 
+  [class*='SocialMediaIcons'] {
+    justify-self: end;
+  }
+
+  .small {
+    color: ${colors.grey[300]};
+    font-size: 12px;
+    text-transform: uppercase;
+
+    &:not(:last-of-type) {
+      /* margin-bottom: 16px; */
+    }
+  }
+
   .made-in {
     font-size: 14px;
+    margin-bottom: ${spacing.l}rem;
+
+    @media screen and (min-width: ${mq.desktop.small}) {
+      margin-bottom: 0;
+      order: 2;
+    }
 
     & .icon {
       font-size: 12px;
@@ -41,14 +67,18 @@ const StyledFooter = styled.footer`
   }
 
   .copyright {
-    p {
-      color: ${colors.grey[300]};
-      font-size: 12px;
-      text-transform: uppercase;
+    margin-bottom: ${spacing.base}rem;
 
-      &:not(:last-of-type) {
-        margin-bottom: 16px;
-      }
+    @media screen and (min-width: ${mq.desktop.small}) {
+      margin-bottom: 0;
+      order: 1;
+    }
+  }
+
+  .motto {
+    @media screen and (min-width: ${mq.desktop.small}) {
+      margin-bottom: 0;
+      order: 3;
     }
   }
 `;
@@ -57,12 +87,32 @@ const FooterContainer = styled(ContentContainer)`
   display: grid;
   padding-top: 80px;
   padding-bottom: 80px;
-  row-gap: ${spacing.l}rem;
+  gap: ${spacing.l}rem;
 
-  @media screen and (min-width: ${mq.desktop.small}) {
-    align-items: flex-end;
-    flex-direction: row;
-    justify-content: space-between;
+  .primary,
+  .secondary {
+    display: grid;
+
+    @media screen and (min-width: ${mq.desktop.small}) {
+      grid-auto-flow: column;
+
+      justify-content: space-between;
+    }
+  }
+
+  .primary {
+    gap: ${spacing.l}rem;
+
+    @media screen and (min-width: ${mq.desktop.small}) {
+      grid-template-columns: 1fr 2fr 1fr;
+      align-items: center;
+    }
+  }
+
+  .secondary {
+    @media screen and (min-width: ${mq.desktop.small}) {
+      align-items: flex-end;
+    }
   }
 `;
 
@@ -80,42 +130,38 @@ const Footer = () => {
   return (
     <StyledFooter>
       <FooterContainer>
-        <Link to="/" className="logo">
-          <Logo />
-        </Link>
-        <ul className="nav">
-          <li className="nav__item">
-            <Link to="/about">
-              <a>About</a>
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link to="/contact">
-              <a>Contact</a>
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link to="/">
-              <a>
+        <div className="primary">
+          <Link to="/" className="logo">
+            <Logo />
+          </Link>
+          <ul className="nav">
+            <li className="nav__item">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="nav__item">
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li className="nav__item">
+              <Link to="/">
                 Download resume{' '}
                 <FontAwesomeIcon icon="download" className="icon" />
-              </a>
-            </Link>
-          </li>
-        </ul>
-        <SocialMediaIcons />
-        <div className="made-in">
-          <span>Made with </span>
-          <span className="icon">
-            <FontAwesomeIcon icon="heart" />
-          </span>
-          <span> in Austin, TX.</span>
+              </Link>
+            </li>
+          </ul>
+          <SocialMediaIcons />
         </div>
-        <div className="copyright">
-          <p>
+        <div className="secondary">
+          <div className="made-in">
+            <span>Made with </span>
+            <span className="icon">
+              <FontAwesomeIcon icon="heart" />
+            </span>
+            <span> in Austin, TX.</span>
+          </div>
+          <p className="small copyright">
             ©<CurrentYear /> {data.site.siteMetadata.author}.
           </p>
-          <p>Soli deo gloria</p>
+          <p className="small motto">Soli deo gloria</p>
         </div>
       </FooterContainer>
     </StyledFooter>
