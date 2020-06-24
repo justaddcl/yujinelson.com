1e9d85394a9f054254836f3507f8566d9d1fb888
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-06-23 20:06:00 -0500

    Text - created p component

---
+++ src/components/styles/Text.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { colors, fonts, mq, spacing } from './theme';
+
+const P = styled.p`
+  color: ${colors.grey[50]};
+  font-family: ${fonts.family.sourceCodePro};
+  line-height: ${spacing.m}rem;
+
+  @media screen and (min-width: ${mq.mobile.large}) {
+    line-height: 32px;
+    max-width: 50rem;
+  }
+
+  @media screen and (min-width: ${mq.desktop.small}) {
+    font-size: 1.125rem;
+  }
+
+  &:not(:last-child) {
+    margin-bottom: ${spacing.l}rem;
+  }
+`;
+
+export default P;
