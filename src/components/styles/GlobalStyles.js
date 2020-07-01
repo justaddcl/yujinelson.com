610092de37b88df08c1b1eac0082843e4b55734e
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-06-30 22:19:47 -0500

    GlobalStyles - default p, default body color

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import 'reset-css';
-import { colors, fonts, mq } from './theme';
+import { colors, fonts, mq, spacing } from './theme';
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -9,7 +9,7 @@ const GlobalStyles = createGlobalStyle`
 
   body {
     background-color: ${colors.purple[800]};
-    color: ${colors.grey[300]};
+    color: ${colors.grey[200]};
     font-family: ${fonts.family.proximaNova};
     font-size: ${fonts.size.mobile}px;
 
@@ -24,6 +24,12 @@ const GlobalStyles = createGlobalStyle`
 
   a {
     text-decoration: none;
+  }
+
+  p {
+    color: ${colors.grey[100]};
+    font-size: ${spacing.base}rem;
+    line-height: 1.5;
   }
 
   .token {
