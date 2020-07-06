2aa5a7fc7ff427915846eab2b4b37da70edbfced
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-07-06 17:35:25 -0500

    index, about - changed to implicit return

--- src/pages/about.js
+++ src/pages/about.js
@@ -42,174 +42,170 @@ const SkillsList = styled.dl`
   }
 `;
 
-const About = ({ data }) => {
-  return (
-    <AboutPage>
-      <Head pageTitle="About me" />
-      <Hero responsiveSplit>
+const About = ({ data }) => (
+  <AboutPage>
+    <Head pageTitle="About me" />
+    <Hero responsiveSplit>
+      <div>
+        <H1>My story</H1>
+        <P>
+          I’m Yuji, a front-end developer and user experience designer,
+          originally from Seattle, WA, who now works and lives in Austin, TX.
+        </P>
+      </div>
+      <div>
+        <Img
+          fluid={data.headshot.childImageSharp.fluid}
+          className="hero-image"
+        />
+      </div>
+    </Hero>
+    <section>
+      <P>
+        From watching my dad work on computers when I was a kid, I cultivated a
+        passion and curiosity for technology which led me to teach myself how to
+        code in middle school. Though I ended up studying Business Information
+        Systems, I wanted to use my coding skills to build products and found my
+        niche in front-end development.
+      </P>
+      <H3>Starting out</H3>
+      <P>
+        I started my career at Boeing as a web developer and was given the
+        opportunity to redesign an enterprise website on mobile communications.
+        Through this project, I updated the front-end framework and rebuilt the
+        user interface to help employees locate and consume critical content.
+        While the overhaul led to increased user engagement and fewer help desk
+        calls, the biggest impact for me was on a personal level. It showed me
+        the power of understanding the user and how employing thoughtful design
+        and well-written code could solve real needs.
+      </P>
+      <P>
+        Over the last few years, I’ve continued to use these skills in the
+        corporate world, but through personal projects, I’ve been able to more
+        fully express my passion for front-end development and UX. Whether, it’s
+        building a landing page for work, or an app on the side for my church to
+        help coordinate events, I’m always looking for ways to improve and
+        deliver my best work yet.{' '}
+      </P>
+      <H3>At the end of the day</H3>
+      <P>
+        While I love what I do, I know life is more than just work and I make
+        time to recharge my passion and creativity through some fun and
+        relaxation. Outside of work, you can find me playing board games with
+        friends, watching Marvel movies with my wife, listening to a Mitch Rapp
+        audiobook, or occasionally DJing a wedding.
+      </P>
+      <Link to="/contact">
+        <Button as="span">
+          Get in touch{' '}
+          <FontAwesomeIcon icon="arrow-right" className="icon--right" />
+        </Button>
+      </Link>
+    </section>
+    <section>
+      <H2>Current role</H2>
+      <ExperienceCard
+        startDate="03/17"
+        rolePrimary="Front-end Developer"
+        roleSecondary="Magpul"
+        logo={data.magpulLogo}
+        roleTeam="Marketing"
+        location="Austin, TX"
+        tags={['dev', 'ux', 'ecommerce']}
+      >
+        <ul className="role-bullets">
+          <li>
+            Develop all Magpul business-to-business (B2B) and
+            business-to-customer (B2C) website content and eCommerce activities
+          </li>
+          <li>
+            Provide oversight, guidance, and act as subject matter expert to all
+            implementation partners on user experience (UX) design and
+            website development
+          </li>
+          <li>
+            Collaborate with the marketing team for website innovation and
+            wireframe design, to build responsive web pages
+          </li>
+          <li>
+            Designed, developed and implemented new website and product pages
+            using HTML, Less, CSS, JavaScript, and Adobe XD which resulted in
+            increased customer engagement and product sales
+          </li>
+          <li>
+            Created website style guide to drive interface consistency and
+            maintain the integrity of the Magpul family of websites
+          </li>
+        </ul>
+      </ExperienceCard>
+      <Link to="/resume">
+        <Button as="span">
+          See resume{' '}
+          <FontAwesomeIcon icon="arrow-right" className="icon--right" />
+        </Button>
+      </Link>
+    </section>
+    <section>
+      <H2>Skills</H2>
+      <SkillsList>
         <div>
-          <H1>My story</H1>
-          <P>
-            I’m Yuji, a front-end developer and user experience designer,
-            originally from Seattle, WA, who now works and lives in Austin, TX.
-          </P>
+          <dt>Languages</dt>
+          <dd>
+            <ul>
+              <li>Javascript (ES6)</li>
+              <li>CSS / Sass / Less / Styled Components</li>
+              <li>HTML / Pug</li>
+              <li>GraphQL</li>
+            </ul>
+          </dd>
         </div>
         <div>
-          <Img
-            fluid={data.headshot.childImageSharp.fluid}
-            className="hero-image"
-          />
+          <dt>Frameworks</dt>
+          <dd>
+            <ul>
+              <li>React</li>
+              <li>Gatsby</li>
+              <li>Prisma</li>
+              <li>Apollo</li>
+              <li>Ruby on Rails</li>
+              <li>Bootstrap</li>
+            </ul>
+          </dd>
         </div>
-      </Hero>
-      <section>
-        <P>
-          From watching my dad work on computers when I was a kid, I cultivated
-          a passion and curiosity for technology which led me to teach myself
-          how to code in middle school. Though I ended up studying Business
-          Information Systems, I wanted to use my coding skills to build
-          products and found my niche in front-end development.
-        </P>
-        <H3>Starting out</H3>
-        <P>
-          I started my career at Boeing as a web developer and was given the
-          opportunity to redesign an enterprise website on mobile
-          communications. Through this project, I updated the front-end
-          framework and rebuilt the user interface to help employees locate and
-          consume critical content. While the overhaul led to increased user
-          engagement and fewer help desk calls, the biggest impact for me was on
-          a personal level. It showed me the power of understanding the user and
-          how employing thoughtful design and well-written code could solve real
-          needs.
-        </P>
-        <P>
-          Over the last few years, I’ve continued to use these skills in the
-          corporate world, but through personal projects, I’ve been able to more
-          fully express my passion for front-end development and UX. Whether,
-          it’s building a landing page for work, or an app on the side for my
-          church to help coordinate events, I’m always looking for ways to
-          improve and deliver my best work yet.{' '}
-        </P>
-        <H3>At the end of the day</H3>
-        <P>
-          While I love what I do, I know life is more than just work and I make
-          time to recharge my passion and creativity through some fun and
-          relaxation. Outside of work, you can find me playing board games with
-          friends, watching Marvel movies with my wife, listening to a Mitch
-          Rapp audiobook, or occasionally DJing a wedding.
-        </P>
-        <Link to="/contact">
-          <Button as="span">
-            Get in touch{' '}
-            <FontAwesomeIcon icon="arrow-right" className="icon--right" />
-          </Button>
-        </Link>
-      </section>
-      <section>
-        <H2>Current role</H2>
-        <ExperienceCard
-          startDate="03/17"
-          rolePrimary="Front-end Developer"
-          roleSecondary="Magpul"
-          logo={data.magpulLogo}
-          roleTeam="Marketing"
-          location="Austin, TX"
-          tags={['dev', 'ux', 'ecommerce']}
-        >
-          <ul className="role-bullets">
-            <li>
-              Develop all Magpul business-to-business (B2B) and
-              business-to-customer (B2C) website content and eCommerce
-              activities
-            </li>
-            <li>
-              Provide oversight, guidance, and act as subject matter expert to
-              all implementation partners on user experience (UX) design and
-              website development
-            </li>
-            <li>
-              Collaborate with the marketing team for website innovation and
-              wireframe design, to build responsive web pages
-            </li>
-            <li>
-              Designed, developed and implemented new website and product pages
-              using HTML, Less, CSS, JavaScript, and Adobe XD which resulted in
-              increased customer engagement and product sales
-            </li>
-            <li>
-              Created website style guide to drive interface consistency and
-              maintain the integrity of the Magpul family of websites
-            </li>
-          </ul>
-        </ExperienceCard>
-        <Link to="/resume">
-          <Button as="span">
-            See resume{' '}
-            <FontAwesomeIcon icon="arrow-right" className="icon--right" />
-          </Button>
-        </Link>
-      </section>
-      <section>
-        <H2>Skills</H2>
-        <SkillsList>
-          <div>
-            <dt>Languages</dt>
-            <dd>
-              <ul>
-                <li>Javascript (ES6)</li>
-                <li>CSS / Sass / Less / Styled Components</li>
-                <li>HTML / Pug</li>
-                <li>GraphQL</li>
-              </ul>
-            </dd>
-          </div>
-          <div>
-            <dt>Frameworks</dt>
-            <dd>
-              <ul>
-                <li>React</li>
-                <li>Gatsby</li>
-                <li>Prisma</li>
-                <li>Apollo</li>
-                <li>Ruby on Rails</li>
-                <li>Bootstrap</li>
-              </ul>
-            </dd>
-          </div>
-          <div>
-            <dt>Tools</dt>
-            <dd>
-              <ul>
-                <li>Git / GitHub</li>
-                <li>ESLint / Prettier</li>
-                <li>Chrome / Firefox Dev Tools</li>
-                <li>Jest</li>
-                <li>Postman</li>
-              </ul>
-            </dd>
-          </div>
-          <div>
-            <dt>Design</dt>
-            <dd>
-              <ul>
-                <li>Adobe XD, Illustrator, Photoshop</li>
-                <li>Mobile-First Responsive Design</li>
-                <li>User Interface & Experience</li>
-                <li>Prototyping</li>
-              </ul>
-            </dd>
-          </div>
-        </SkillsList>
-        <Link to="/resume">
-          <Button as="span">
-            See resume{' '}
-            <FontAwesomeIcon icon="arrow-right" className="icon--right" />
-          </Button>
-        </Link>
-      </section>
-      <ContactCard />
-    </AboutPage>
-  );
-};
+        <div>
+          <dt>Tools</dt>
+          <dd>
+            <ul>
+              <li>Git / GitHub</li>
+              <li>ESLint / Prettier</li>
+              <li>Chrome / Firefox Dev Tools</li>
+              <li>Jest</li>
+              <li>Postman</li>
+            </ul>
+          </dd>
+        </div>
+        <div>
+          <dt>Design</dt>
+          <dd>
+            <ul>
+              <li>Adobe XD, Illustrator, Photoshop</li>
+              <li>Mobile-First Responsive Design</li>
+              <li>User Interface & Experience</li>
+              <li>Prototyping</li>
+            </ul>
+          </dd>
+        </div>
+      </SkillsList>
+      <Link to="/resume">
+        <Button as="span">
+          See resume{' '}
+          <FontAwesomeIcon icon="arrow-right" className="icon--right" />
+        </Button>
+      </Link>
+    </section>
+    <ContactCard />
+  </AboutPage>
+);
 
 export const query = graphql`
   query {
--- src/pages/index.js
+++ src/pages/index.js
@@ -38,101 +38,96 @@ const ButtonContainer = styled.ul`
   }
 `;
 
-const Home = ({ data }) => {
-  return (
-    <HomePage>
-      <Head />
-      <Hero>
-        <Greeting />
-        <H1>I'm Yuji Nelson.</H1>
-        <P>
-          A front-end developer and user experience (UX) designer with a knack
-          for business and marketing strategy.
-        </P>
-        <Img
-          fluid={data.portrait.childImageSharp.fluid}
-          className="hero-image"
-        />
-      </Hero>
-      <section id="intro">
-        <H2>Introduction</H2>
-        <P>
-          I design and build mobile-first, responsive websites and web apps
-          using modern HTML, CSS, and JavaScript. As a developer, I love solving
-          puzzles and constantly look for ways to better apply my code and
-          deliver a better product. As a designer, my passion lies in delivering
-          intuitive and delightful experiences through well crafted, beautiful
-          design. Currently, I work at Magpul in Austin, TX.
-        </P>
-        <Link to="/about">
-          <Button as="span">
-            Read my story{' '}
-            <FontAwesomeIcon icon="arrow-right" className="icon--right" />
-          </Button>
-        </Link>
-      </section>
-      <section id="experience">
-        <H2>Experience</H2>
-        <ExperienceCard
-          startDate="03/17"
-          rolePrimary="Front-end Developer"
-          roleSecondary="Magpul"
-          logo={data.magpulLogo}
-          roleTeam="Marketing"
-          location="Austin, TX"
-          tags={['dev', 'ux', 'ecommerce']}
-        />
-        <ExperienceCard
-          startDate="07/16"
-          endDate="06/17"
-          rolePrimary="Company Name"
-          roleSecondary="Lorem ipsum dolor sit amet rotation program"
-          logo={data.boeingLogo}
-          location="Bellevue, WA"
-          roles={[
-            {
-              title: 'Position Title',
-              team: 'Enterprise Communications',
-              startDate: '07/16',
-              endDate: '06/17',
-            },
-            {
-              title: 'Position Title',
-              team: 'Enterprise Communications',
-              startDate: '07/16',
-              endDate: '06/17',
-            },
-            {
-              title: 'Position Title',
-              team: 'Enterprise Communications',
-              startDate: '07/16',
-              endDate: '06/17',
-            },
-          ]}
-          tags={['dev', 'ux']}
-        />
-        <ButtonContainer>
-          <li>
-            <Link to="/resume">
-              <Button as="span">
-                See resume{' '}
-                <FontAwesomeIcon icon="arrow-right" className="icon--right" />
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about#skills">
-              <Button as="span">
-                My skillset{' '}
-                <FontAwesomeIcon icon="arrow-right" className="icon--right" />
-              </Button>
-            </Link>
-          </li>
-        </ButtonContainer>
-      </section>
-    </HomePage>
-  );
-};
+const Home = ({ data }) => (
+  <HomePage>
+    <Head />
+    <Hero>
+      <Greeting />
+      <H1>I'm Yuji Nelson.</H1>
+      <P>
+        A front-end developer and user experience (UX) designer with a knack for
+        business and marketing strategy.
+      </P>
+      <Img fluid={data.portrait.childImageSharp.fluid} className="hero-image" />
+    </Hero>
+    <section id="intro">
+      <H2>Introduction</H2>
+      <P>
+        I design and build mobile-first, responsive websites and web apps using
+        modern HTML, CSS, and JavaScript. As a developer, I love solving puzzles
+        and constantly look for ways to better apply my code and deliver a
+        better product. As a designer, my passion lies in delivering intuitive
+        and delightful experiences through well crafted, beautiful design.
+        Currently, I work at Magpul in Austin, TX.
+      </P>
+      <Link to="/about">
+        <Button as="span">
+          Read my story{' '}
+          <FontAwesomeIcon icon="arrow-right" className="icon--right" />
+        </Button>
+      </Link>
+    </section>
+    <section id="experience">
+      <H2>Experience</H2>
+      <ExperienceCard
+        startDate="03/17"
+        rolePrimary="Front-end Developer"
+        roleSecondary="Magpul"
+        logo={data.magpulLogo}
+        roleTeam="Marketing"
+        location="Austin, TX"
+        tags={['dev', 'ux', 'ecommerce']}
+      />
+      <ExperienceCard
+        startDate="07/16"
+        endDate="06/17"
+        rolePrimary="Company Name"
+        roleSecondary="Lorem ipsum dolor sit amet rotation program"
+        logo={data.boeingLogo}
+        location="Bellevue, WA"
+        roles={[
+          {
+            title: 'Position Title',
+            team: 'Enterprise Communications',
+            startDate: '07/16',
+            endDate: '06/17',
+          },
+          {
+            title: 'Position Title',
+            team: 'Enterprise Communications',
+            startDate: '07/16',
+            endDate: '06/17',
+          },
+          {
+            title: 'Position Title',
+            team: 'Enterprise Communications',
+            startDate: '07/16',
+            endDate: '06/17',
+          },
+        ]}
+        tags={['dev', 'ux']}
+      />
+      <ButtonContainer>
+        <li>
+          <Link to="/resume">
+            <Button as="span">
+              See resume{' '}
+              <FontAwesomeIcon icon="arrow-right" className="icon--right" />
+            </Button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/about#skills">
+            <Button as="span">
+              My skillset{' '}
+              <FontAwesomeIcon icon="arrow-right" className="icon--right" />
+            </Button>
+          </Link>
+        </li>
+      </ButtonContainer>
+    </section>
+  </HomePage>
+);
 
 export const query = graphql`
   query {
