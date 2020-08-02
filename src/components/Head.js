2355b933982742d94b44d9d0cd9677f0ab4775d1
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-08-02 15:43:44 -0500

    iPhone X notch safe-area fix for Safari

--- src/components/Head.js
+++ src/components/Head.js
@@ -17,6 +17,10 @@ const Head = ({ pageTitle = '' }) => {
       <title>
         {`${pageTitle ? `${pageTitle} - ` : ''}${data.site.siteMetadata.title}`}
       </title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, shrink-to-fit=no, viewport-fit=cover"
+      />
       <script>
         {`
           (function(d) {
