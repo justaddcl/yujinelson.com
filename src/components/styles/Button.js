461aa2caa483775b747264c6773534e41d7e3519
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-08-08 15:53:16 -0500

    Button - used button mixin styles

--- src/components/styles/Button.js
+++ src/components/styles/Button.js
@@ -1,23 +1,9 @@
 import styled from 'styled-components';
-import { colors, mq, spacing } from './theme';
+import { spacing } from './theme';
+import mixins from './mixins';
 
 const Button = styled.button`
-  background: ${(props) => (props.primary ? colors.purple[500] : 'none')};
-  border: ${(props) =>
-    props.primary ? 'none' : `1px solid ${colors.purple[500]}`};
-  border-radius: 2px;
-  color: ${(props) => (props.primary ? colors.grey[50] : colors.grey[200])};
-  cursor: pointer;
-  display: inline-block;
-  font: inherit;
-  padding: ${(props) =>
-    props.large
-      ? `${spacing.base}rem ${spacing.m}rem`
-      : `${spacing.xs}rem ${spacing.s}rem`};
-
-  @media screen and (min-width: ${mq.mobile.small}) {
-    padding: ${spacing.base}rem ${spacing.m}rem;
-  }
+  ${mixins.button};
 
   .icon {
     &--right {
