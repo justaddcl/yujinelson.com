e0df2ef1c87409330cb0181b0dcd14b30b47a2fa
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-08-08 15:52:36 -0500

    ResumeLink - updated default text, added conditional button styling

--- src/components/ResumeLink.js
+++ src/components/ResumeLink.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import resume from '../assets/pdf/Yuji-Nelson-Resume-2020.pdf';
+import { spacing, mq } from './styles/theme';
+import mixins from './styles/mixins';
 
 const StyledResumeLink = styled.a`
+  ${(props) => (props.button ? mixins.button : '')};
+
+  @media screen and (min-width: ${mq.mobile.small}) {
+    padding: ${(props) =>
+      props.button ? `${spacing.xs}rem ${spacing.base}rem` : ''};
+  }
+
   & .icon {
     font-size: 0.875rem;
   }
 `;
 
-const ResumeLink = ({ children }) => (
-  <StyledResumeLink href={resume} target="_blank" rel="noopener noreferrer">
-    {children || (
-      <>
-        Download resume <FontAwesomeIcon icon="download" className="icon" />
-      </>
-    )}
+const ResumeLink = ({ button, children }) => (
+  <StyledResumeLink
+    href={resume}
+    target="_blank"
+    rel="noopener noreferrer"
+    button={button}
+  >
+    {children || <>Resume</>}
   </StyledResumeLink>
 );
 
