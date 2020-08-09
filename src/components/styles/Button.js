c06f963717249fa5d6f6b952645b9227b21f1bd6
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-08-09 16:04:13 -0500

    deleted Button component - use mix-in styles instead

--- src/components/styles/Button.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { spacing } from './theme';
-import mixins from './mixins';
-
-const Button = styled.button`
-  ${mixins.button};
-`;
-
-export default Button;
