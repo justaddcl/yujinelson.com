41d3846f5b5221fd2c163a51a926be49766dd876
Yuji Nelson <k.yuji.nelson@gmail.com>
2022-10-14 14:18:55 +0100

    Updates gatsby-node to use named export instead of default

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -3,7 +3,12 @@ import type { GatsbyNode } from 'gatsby';
 const resume =
   '/static/Yuji-Nelson-CV-2022-21e9515412f6493c7053aaff5ea45134.pdf';
 
-const createPages: GatsbyNode['createPages'] = ({ actions }) => {
+/**
+ * Gatsby complains if we use a default export, so disabling the eslint rule to make sure the file exports
+ * match what Gatsby is expecting
+ */
+// eslint-disable-next-line import/prefer-default-export
+export const createPages: GatsbyNode['createPages'] = ({ actions }) => {
   const { createRedirect } = actions;
   createRedirect({
     fromPath: '/resume',
@@ -12,5 +17,3 @@ const createPages: GatsbyNode['createPages'] = ({ actions }) => {
     redirectInBrowser: true,
   });
 };
-
-export default createPages;
